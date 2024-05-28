56b8e2706e5db740aacffa3a11479161a0b8599c
luigithiago <luigithiago13@hotmail.com>
2024-05-27 22:53:15 -0300

    Commit do projeto de simulação de conta bancária através do Terminal/Console

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        //TO DO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela classe Scanner os valores digitados no terminal
+
+        //Exibir a mensagem da conta criada
+        
+        double saldo = 2368.89;
+
+        Scanner scanner = new Scanner(System.in); //.useLocale(Locale.US);
+
+        System.out.println("Por favor, digite sua conta");
+        int numero = scanner.nextInt();
+     
+        System.out.println("Por favor, digite sua agência");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite seu nome");
+        String nomeCliente = scanner.next();
+        
+       
+        System.out.println("Olá, " + nomeCliente + "! Obrigado por criar uma conta em nosso banco! Sua agência é " + agencia + ", sua conta é " + numero + " e seu saldo de " + saldo + " já está disponível para saque.");
+               
     }
 }
